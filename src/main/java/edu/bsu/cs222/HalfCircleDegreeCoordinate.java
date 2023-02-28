15fd1a12b5f8d009cec17b6627bb4c6558503888
Justyn Fox <jjfox@bsu.edu>
2023-02-28 14:51:22 -0500

    Add equals comparison for HalfCircleDegreeCoordinate.

---
+++ src/main/java/edu/bsu/cs222/HalfCircleDegreeCoordinate.java
@@ -0,0 +1,29 @@
+package edu.bsu.cs222;
+
+public class HalfCircleDegreeCoordinate {
+
+    // Resolution goes down to 1/1000 of an arcsecond
+    // (two inches long/lat or 1/15 of a millisecond of rotation).
+    // This should be adequate for this project.
+    private static final int MAX_UNITS = 180*60*60*1000;
+    private static final int UNITS_PER_DEGREE = 60*60*1000;
+    private static final int UNITS_PER_ARCMINUTE = 60*1000;
+    private static final int UNITS_PER_ARCSECOND = 1000;
+
+    private final int units;
+
+    public HalfCircleDegreeCoordinate(int degrees, int arcminutes, double arcseconds) {
+        int unitsNoMod = UNITS_PER_DEGREE*degrees
+                + UNITS_PER_ARCMINUTE*arcminutes
+                + (int)(UNITS_PER_ARCSECOND*arcseconds);
+        this.units = unitsNoMod % MAX_UNITS;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof HalfCircleDegreeCoordinate other) {
+            return this.units == other.units;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/edu/bsu/cs222/HalfCircleDegreeCoordinateTest.java
@@ -0,0 +1,74 @@
+package edu.bsu.cs222;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HalfCircleDegreeCoordinateTest {
+
+    // This suppression is needed to verify this intended functionality where anything of another
+    // type is not equal.
+    @SuppressWarnings("EqualsBetweenInconvertibleTypes")
+    @Test
+    public void testEqualsNonHalfCircleDegreeCoordinateNotEqual() {
+        HalfCircleDegreeCoordinate coordinate = new HalfCircleDegreeCoordinate(45, 30, 30.0);
+        String randomObject = "";
+        boolean result = coordinate.equals(randomObject);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testEqualsSameUnitValueIsEqual() {
+        HalfCircleDegreeCoordinate coordinate0 = new HalfCircleDegreeCoordinate(45, 30, 30.0);
+        HalfCircleDegreeCoordinate coordinate1 = new HalfCircleDegreeCoordinate(45, 30, 30.0);
+        boolean result = coordinate0.equals(coordinate1);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void testEqualsDifferentDegreeNotEqual() {
+        HalfCircleDegreeCoordinate coordinate0 = new HalfCircleDegreeCoordinate(45, 30, 30.0);
+        HalfCircleDegreeCoordinate coordinate1 = new HalfCircleDegreeCoordinate(44, 30, 30.0);
+        boolean result = coordinate0.equals(coordinate1);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testEqualsDifferentArcMinuteNotEqual() {
+        HalfCircleDegreeCoordinate coordinate0 = new HalfCircleDegreeCoordinate(45, 30, 30.0);
+        HalfCircleDegreeCoordinate coordinate1 = new HalfCircleDegreeCoordinate(45, 29, 30.0);
+        boolean result = coordinate0.equals(coordinate1);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testEqualsDifferentArcSecondNotEqual() {
+        HalfCircleDegreeCoordinate coordinate0 = new HalfCircleDegreeCoordinate(45, 30, 30.0);
+        HalfCircleDegreeCoordinate coordinate1 = new HalfCircleDegreeCoordinate(45, 30, 29.0);
+        boolean result = coordinate0.equals(coordinate1);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testEqualsNegationNotEqual() {
+        HalfCircleDegreeCoordinate coordinate0 = new HalfCircleDegreeCoordinate(45, 30, 30.0);
+        HalfCircleDegreeCoordinate coordinate1 = new HalfCircleDegreeCoordinate(-45, 30, 30.0);
+        boolean result = coordinate0.equals(coordinate1);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testEqualsDifferenceBelowResolutionEqual() {
+        HalfCircleDegreeCoordinate coordinate0 = new HalfCircleDegreeCoordinate(45, 30, 30.0);
+        HalfCircleDegreeCoordinate coordinate1 = new HalfCircleDegreeCoordinate(45, 30, 30.0001);
+        boolean result = coordinate0.equals(coordinate1);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void testEqualsDifferenceIsModulusEqual() {
+        HalfCircleDegreeCoordinate coordinate0 = new HalfCircleDegreeCoordinate(45, 30, 30.0);
+        HalfCircleDegreeCoordinate coordinate1 = new HalfCircleDegreeCoordinate(45+180, 30, 30.0);
+        boolean result = coordinate0.equals(coordinate1);
+        Assertions.assertTrue(result);
+    }
+}
