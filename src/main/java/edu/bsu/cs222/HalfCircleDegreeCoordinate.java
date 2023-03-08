e7031b58888e8eaacee4a24f2e6d7c068d5253fc
Lee Blake <charlotte.blake@bsu.edu>
2023-03-08 12:30:01 -0500

    Make HalfCircleDegreeCoordinate non-modular
    Fundamentally, the modular behavior of a full circle does
    not extend to a half-circle. Most notably, a half-circle
    has two distinct poles, and halving the modulus makes these
    poles equal to each other. Therefore, our implementation
    had a serious bug in that the two points the furthest away
    from each other were treated as equal. While it would be
    possible to refactor to modulus + 1 and make them non-equal,
    there is no benefit to this. Thus, dropping the modulus
    entirely is probably a better choice here, as it simplifies
    conversions by the formatters that will be implemented. It
    therefore is necessary to validate the degree ranges for
    the new form of half-circle coordinates.For the full-circle
    coordinates, modular behavior is still desirable, and thus
    no changes are needed at this time.

--- src/main/java/edu/bsu/cs222/HalfCircleDegreeCoordinate.java
+++ src/main/java/edu/bsu/cs222/HalfCircleDegreeCoordinate.java
@@ -5,7 +5,6 @@ public class HalfCircleDegreeCoordinate {
     // Resolution goes down to 1/1000 of an arcsecond
     // (two inches long/lat or 1/15 of a millisecond of Earth's rotation).
     // This should be adequate for this project.
-    private static final long MAX_UNITS = 180*60*60*1000;
     private static final long UNITS_PER_DEGREE = 60*60*1000;
     private static final long UNITS_PER_ARCMINUTE = 60*1000;
     private static final long UNITS_PER_ARCSECOND = 1000;
@@ -13,12 +12,37 @@ public class HalfCircleDegreeCoordinate {
     private final long units;
 
     public HalfCircleDegreeCoordinate(int degrees, int arcminutes, double arcseconds) {
+        if(!coordinatesAreValid(degrees, arcminutes, arcseconds)) {
+            throw new IllegalArgumentException(
+                    "Degree coordinates '"
+                            + degrees + ","
+                            + arcminutes + ","
+                            + arcseconds + "'"
+                            + "were not valid half-circle coordinates! Arcminutes and arcseconds must be in [0,60) "
+                            + " and degrees in (-90,90) except for coordinates <90,0,0> and <-90,0,0>."
+            );
+        }
         int sign = degrees == 0 ? 1 : degrees / Math.abs(degrees);
         long unitsNoModNoSign = sign*UNITS_PER_DEGREE*degrees
                 + UNITS_PER_ARCMINUTE *arcminutes
                 + (long)(UNITS_PER_ARCSECOND*arcseconds);
-        long unitsBeforePositiveNormalization = (sign*unitsNoModNoSign) % MAX_UNITS;
-        this.units = (unitsBeforePositiveNormalization + MAX_UNITS) % MAX_UNITS;
+        this.units = sign*unitsNoModNoSign;
+    }
+
+    private boolean coordinatesAreValid(int degrees, int arcminutes, double arcseconds) {
+        if(
+                degrees > 90 || degrees < -90
+                || arcminutes > 59 || arcminutes < 0
+                || arcseconds >= 60 || arcseconds < 0
+        ) {
+            return false;
+        }
+        else if(degrees == 90 || degrees == -90) {
+            return arcminutes == 0 && arcseconds == 0;
+        }
+        else {
+            return true;
+        }
     }
 
     @Override
@@ -28,4 +52,5 @@ public class HalfCircleDegreeCoordinate {
         }
         return false;
     }
+
 }
