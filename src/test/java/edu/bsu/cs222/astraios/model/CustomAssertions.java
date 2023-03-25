659077994e7ad745f2393397dc4de912fe6a9bf1
Lee Blake <charlotte.blake@bsu.edu>
2023-03-25 10:32:00 -0400

    Add CustomAssertions::assertBounded
    A number of tests for calculating the position of objects and the Sun
    will require checking that the predictions are reasonably close to
    reality. As this will be handled by asserting the error is bounded, and
    as such assertions will occur in multiple classes, it is helpful to
    create a static helper method in an appropriately-named class - with
    custom messaging displaying the actual deviation - in preparation for
    these unit tests.

---
+++ src/test/java/edu/bsu/cs222/astraios/model/CustomAssertions.java
@@ -0,0 +1,16 @@
+package edu.bsu.cs222.astraios.model;
+
+import org.junit.jupiter.api.Assertions;
+
+public class CustomAssertions {
+
+    public static void assertBounded(AngularDistance bound, AngularDistance actual) {
+        String messageIfAssertionFails = "Bounded assertion failed: "
+                + actual.toString()
+                + " was not less than the bound "
+                + bound.toString()
+                + "!";
+        boolean isBounded = actual.lessThan(bound);
+        Assertions.assertTrue(isBounded,messageIfAssertionFails);
+    }
+}
