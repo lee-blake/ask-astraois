cdfd9e14fff62eeb00fa320958857cbf7e6ab156
Lee Blake <charlotte.blake@bsu.edu>
2023-04-07 14:34:11 -0400

    Revert AskAstraios to pre-'--example'
    The last few commits introduced a major problem into the program -
    namely, it no longer automatically answers invalid inputs with
    appropriate help messages. This is a result of calling parseResult and
    not execute - something that fundamentally must be done to check for
    '--example' prior to execution. However, even wrapping this call to
    defer back to picocli's handlers does not fix this problem, as that will
     cause the '--example' token to be ignored in many of the circumstances
     where that option is most useful. Furthermore, the code required to
     make this work is quite ugly and is not clean conceptually. As a
     result, it is probably best if we discard the '--example' option and
     just direct the users to the 'example' subcommand.

--- src/main/java/edu/bsu/cs222/astraios/cli/commands/AskAstraios.java
+++ src/main/java/edu/bsu/cs222/astraios/cli/commands/AskAstraios.java
@@ -5,10 +5,6 @@ import org.fusesource.jansi.AnsiConsole;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
-import picocli.CommandLine.ParseResult;
-import picocli.CommandLine.ScopeType;
-
-import java.util.List;
 
 @Command(
         name = "ask-astraios",
@@ -25,8 +21,6 @@ import java.util.List;
 )
 public class AskAstraios {
 
-    private CommandLine commandLine;
-
     @Option(
             names = {"-h", "--help"},
             usageHelp = true,
@@ -34,61 +28,12 @@ public class AskAstraios {
     )
     private boolean helpRequested;
 
-    @Option(
-            names = {"--example"},
-            description = "Get examples for the subcommand. Equivalent to 'example <subcommand>'. "
-                    + "Must be run with a subcommand.",
-            scope = ScopeType.INHERIT,
-            help = true
-    )
-    private boolean exampleRequested;
-
     public static void main(String[] args) {
         AnsiConsole.systemInstall();
-        AskAstraios astraios = new AskAstraios();
-        astraios.commandLine = new CommandLine(astraios)
-                .setExecutionExceptionHandler(new CLIExceptionMessageHandler());
-        ParseResult parseResult = astraios.commandLine.parseArgs(args);
-        int exitCode;
-        if(astraios.exampleRequested) {
-            exitCode = astraios.executeExampleOption(parseResult);
-        }
-        else {
-            exitCode = astraios.executeNormalLogic(args);
-        }
+        int exitCode = new CommandLine(new AskAstraios())
+                .setExecutionExceptionHandler(new CLIExceptionMessageHandler())
+                .execute(args);
         AnsiConsole.systemUninstall();
         System.exit(exitCode);
-    }
-
-    private int executeExampleOption(ParseResult parseResult) {
-        List<CommandLine.ParseResult> subcommandsParsed = parseResult.subcommands();
-        if(subcommandsParsed.isEmpty()) {
-            this.printInvalidSubcommandError();
-            return 1;
-        }
-        else {
-            // Currently, the program only has one layer of subcommands. We may therefore assume that this
-            // list is of length 1 and simply unwrap it to a single String. If this changes, the necessary refactor
-            // of ExampleTextConverter will necessarily have it take an array of Strings. In that case, we will
-            // refactor this to convert the array of subcommands to an array of Strings before right-joining the
-            // resulting array of name Strings to ["example"].
-            String subcommandWhereThisWasCalledFrom = subcommandsParsed.get(0).commandSpec().name();
-            String[] argsToExecuteExampleCorrectly = new String[]{"example", subcommandWhereThisWasCalledFrom};
-            return this.commandLine.execute(argsToExecuteExampleCorrectly);
-        }
-    }
-
-    private void printInvalidSubcommandError() {
-        this.commandLine.getErr().println(
-                this.commandLine.getColorScheme()
-                        .errorText(
-                                "The '--example' option must be run with a valid subcommand! "
-                                        + "See --help for a list of subcommands."
-                        )
-        );
-    }
-
-    private int executeNormalLogic(String[] args) {
-        return this.commandLine.execute(args);
     }
 }
