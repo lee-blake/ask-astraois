bf68be3b6eee46ca54b6cb672443358019a32672
Lee Blake <charlotte.blake@bsu.edu>
2023-04-08 08:53:35 -0400

    Improve ExampleCommand's handling of invalid input

--- src/main/java/edu/bsu/cs222/astraios/cli/commands/ExampleCommand.java
+++ src/main/java/edu/bsu/cs222/astraios/cli/commands/ExampleCommand.java
@@ -1,9 +1,12 @@
 package edu.bsu.cs222.astraios.cli.commands;
 
 import edu.bsu.cs222.astraios.cli.converters.ExampleTextConverter;
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Spec;
+import picocli.CommandLine.Model.CommandSpec;
 
 import java.util.concurrent.Callable;
 
@@ -13,6 +16,9 @@ import java.util.concurrent.Callable;
                 "its variables."
 )
 public class ExampleCommand implements Callable<Integer> {
+
+    @Spec
+    CommandSpec specForThisInvocation;
 
     @Option(
             names = {"-h", "--help"},
@@ -29,9 +35,26 @@ public class ExampleCommand implements Callable<Integer> {
 
     @Override
     public Integer call() {
+        try {
+            this.printExamplesForSubcommand();
+            return 0;
+        }
+        catch(IllegalArgumentException ex) {
+            // We could make a "converter" to validate input instead, but this is the way picocli recommends.
+            // Also, if we were to implement a different view, there would be no need for this - input could be
+            // restricted to a set. Therefore, this message should live here.
+            throw new CommandLine.ParameterException(
+                    specForThisInvocation.commandLine(),
+                    "'"
+                            + subcommand
+                            + "' is not a recognized subcommand! Use 'ask-astraios --help' for a list of subcommands."
+                    );
+        }
+    }
+
+    private void printExamplesForSubcommand() {
         ExampleTextConverter converter = new ExampleTextConverter();
-        String textPrint = converter.getExampleStringForSubcommand(subcommand);
+        String textPrint = converter.getExampleStringForSubcommand(this.subcommand);
         System.out.println(textPrint);
-        return 0;
     }
 }
