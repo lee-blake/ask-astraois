fbaa816c655904a4ec193dbdec06a9f8585f70ca
Lee Blake <charlotte.blake@bsu.edu>
2023-04-10 13:08:43 -0400

    Improve testing for CLIExceptionMessageFactory

--- src/test/java/edu/bsu/cs222/astraios/cli/converters/CLIExceptionMessageFactoryTest.java
+++ src/test/java/edu/bsu/cs222/astraios/cli/converters/CLIExceptionMessageFactoryTest.java
@@ -13,4 +13,15 @@ public class CLIExceptionMessageFactoryTest {
         String expected = "One or more requested journal entries could not be retrieved:\nx";
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testGetMessageThrowsExceptionWhenNonRecognizedException() {
+        CLIExceptionMessageFactory factory = new CLIExceptionMessageFactory();
+        // IllegalThreadStateException is sufficiently obscure for testing - this program is single-threaded and
+        // therefore would require an entire restructure for this test to become invalid.
+        Assertions.assertThrows(
+                IllegalThreadStateException.class,
+                () -> factory.translateExceptionToErrorMessage(new IllegalThreadStateException())
+        );
+    }
 }
