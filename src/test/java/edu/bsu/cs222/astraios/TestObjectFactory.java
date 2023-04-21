c3036f6438d0155f126207ba77a58330b945746c
Lee Blake <charlotte.blake@bsu.edu>
2023-04-21 08:54:46 -0400

    Simplify construction in TestObjectFactory

--- src/test/java/edu/bsu/cs222/astraios/TestObjectFactory.java
+++ src/test/java/edu/bsu/cs222/astraios/TestObjectFactory.java
@@ -13,33 +13,33 @@ public class TestObjectFactory {
     public static class AstronomicalObjects {
 
         public static AstronomicalObject buildM13Object() {
-            HourCoordinate m13RightAscension = new HourCoordinate(16, 41, 41.24);
-            HalfCircleDegreeCoordinate m13Declination = new HalfCircleDegreeCoordinate(36, 27, 35.5);
-            RightAscensionDeclinationCoordinates m13Coords = new RightAscensionDeclinationCoordinates(
-                    m13RightAscension,
-                    m13Declination
+            return new AstronomicalObject(
+                    "M13",
+                    new RightAscensionDeclinationCoordinates(
+                            new HourCoordinate(16, 41, 41.24),
+                            new HalfCircleDegreeCoordinate(36, 27, 35.5)
+                    )
             );
-            return new AstronomicalObject("M13", m13Coords);
         }
 
         public static AstronomicalObject buildM31Object() {
-            HourCoordinate m31RightAscension = new HourCoordinate(0, 42, 44.30);
-            HalfCircleDegreeCoordinate m31Declination = new HalfCircleDegreeCoordinate(41, 16, 9);
-            RightAscensionDeclinationCoordinates m31Coords = new RightAscensionDeclinationCoordinates(
-                    m31RightAscension,
-                    m31Declination
+            return new AstronomicalObject(
+                    "M31",
+                    new RightAscensionDeclinationCoordinates(
+                            new HourCoordinate(0, 42, 44.30),
+                            new HalfCircleDegreeCoordinate(41, 16, 9)
+                    )
             );
-            return new AstronomicalObject("M31", m31Coords);
         }
 
         public static AstronomicalObject buildM83Object() {
-            HourCoordinate m83RightAscension = new HourCoordinate(13, 37, 00.9);
-            HalfCircleDegreeCoordinate m83Declination = new HalfCircleDegreeCoordinate(-29, 51, 57);
-            RightAscensionDeclinationCoordinates m83Coords = new RightAscensionDeclinationCoordinates(
-                    m83RightAscension,
-                    m83Declination
+            return new AstronomicalObject(
+                    "M83",
+                    new RightAscensionDeclinationCoordinates(
+                            new HourCoordinate(13, 37, 00.9),
+                            new HalfCircleDegreeCoordinate(-29, 51, 57)
+                    )
             );
-            return new AstronomicalObject("M83", m83Coords);
         }
     }
 
